b3b2b5f836903772f354622d71ba8cc9025f723d
msmith <msmith@msmiths-macbook-pro-17.local>
2009-11-25 10:28:35 -0600

    add uninstall command

---
+++ .ale/installed/ale/commands/uninstall.py
@@ -0,0 +1,19 @@
+import sys, os, re, logging
+
+from ale.base import Command
+import fnmatch, shutil
+
+installedPath = os.path.join(os.path.realpath(os.curdir), '.ale/installed')
+
+class UnInstall(Command):
+    name = 'uninstall'
+    shorthelp = 'uninstall <command>'
+            
+    def execute(self, args=None):
+        if not args:
+            logging.error('specify a command to uninstall.')
+            return
+        command = args[0]
+        sourceTree = os.path.join(installedPath, command)
+        print 'Uninstalling, deleting %s' % (sourceTree)
+        shutil.rmtree(sourceTree)
