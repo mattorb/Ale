bd90c605a58d5c3947d13c8caa7254aece9f1208
msmith <msmith@msmiths-macbook-pro-17.local>
2009-11-25 15:00:40 -0600

    add helloworldwebapp

--- .ale/notinstalled/createapp/commands/createapp.py
+++ .ale/notinstalled/createapp/commands/createapp.py
@@ -5,21 +5,32 @@ import logging
 
 class CreateAppCommand(Command):
     name = 'createapp'
-    shorthelp = 'create the gae hello world app in the current directory from template'
+    shorthelp = 'createapp [templatename]  -- create the gae hello world app in the current directory from template'
 
     def execute(self, args=None):
-        logging.info('creating ./helloworld.py')
-        FILE = open('./helloworld.py', 'w')
-        FILE.write("""
+        if not args:
+            print self.shorthelp
+            print 'available app templates:'
+            print 'helloworld'
+            print 'helloworldwebapp'
+        else:
+            if args[0].lower() != 'helloworld' and args[0].lower() != 'helloworldwebapp':
+                print 'Unknown app name %s' % args[0]
+                return
+            
+            if args[0].lower() == 'helloworld':
+                    logging.info('creating ./helloworld.py')
+                    FILE = open('./helloworld.py', 'w')
+                    FILE.write("""
 print 'Content-Type: text/plain'
 print ''
 print 'Hello, world!  This is a bare bones app engine application'
 """)
-        FILE.close()
+                    FILE.close()
 
-        logging.info('creating ./app.yaml')
-        FILE = open('./app.yaml', 'w')
-        FILE.write("""
+                    logging.info('creating ./app.yaml')
+                    FILE = open('./app.yaml', 'w')
+                    FILE.write("""
 application: helloworld
 version: 1
 runtime: python
@@ -28,7 +39,43 @@ api_version: 1
 handlers:
 - url: /.*
   script: helloworld.py        
+            """)
+                    FILE.close()                
+            elif args[0].lower() == 'helloworldwebapp':
+                    logging.info('creating ./helloworld.py')
+                    FILE = open('./helloworld.py', 'w')
+                    FILE.write("""
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp World!')
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
 """)
-        FILE.close()
-        
-        logging.info('All Done!   Run "dev_appserver ." to start the local server, then browser to http://localhost:8080')
+                    FILE.close()
+
+                    logging.info('creating ./app.yaml')
+                    FILE = open('./app.yaml', 'w')
+                    FILE.write("""
+application: helloworldwebapp
+version: 1
+runtime: python
+api_version: 1
+
+handlers:
+- url: /.*
+  script: helloworld.py        
+""")
+                    FILE.close()             
+            logging.info('All Done!   Run "dev_appserver ." to start the local server, then browse to http://localhost:8080   Webapp documentation is at http://code.google.com/appengine/docs/python/gettingstarted/usingwebapp.html')
