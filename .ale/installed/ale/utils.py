74934ea5ccd7e2811af3f4ef02a5c33ce72d996d
msmith <msmith@msmiths-macbook-pro-17.local>
2009-11-30 12:08:52 -0600

    working on pyflakes install

--- .ale/installed/ale/utils.py
+++ .ale/installed/ale/utils.py
@@ -2,6 +2,8 @@
 # encoding: utf-8
 import sys
 import os
+from shutil import move
+from aleconfig import *
 
 # just here until we move everything up to python 2.6 or 3.0
 def find(f, seq):
@@ -13,3 +15,15 @@ def mkdir(dirname):
     if not os.path.isdir(dirname):
         os.mkdir(dirname)
 
+def downloadAndExtract(remotePath, extractPath):
+    mkdir(alePath('tmp'))
+    localDlPath = os.path.join(alePath('tmp'), 'tmp.tar.gz')
+    curlCmd = 'curl -o %s %s' % (localDlPath, remotePath)
+    os.system(curlCmd)
+    mkdir(extractPath)
+    extractFile = os.path.join(extractPath, 'tmp.tar.gz')
+    move(localDlPath, extractFile)
+    prevCwd = os.getcwd()
+    os.chdir(extractPath)
+    os.system('gzip -dc %s | tar xf -' % extractFile)
+    os.chdir(prevCwd)
--- .ale/notinstalled/pyflakes/commands/pyflakes.py
+++ .ale/notinstalled/pyflakes/commands/pyflakes.py
@@ -14,18 +14,6 @@ class PyFlakesCommand(Command):
         print 'should run pyflakes'
 
     def install(self, args=None):
-        pyflakesversion = 'pyflakes-0.3.0'
-        pyflakesFile = '%s.tar.gz' % pyflakesversion
+        extractPath = os.path.join(alePath('installed'), 'pyflakes-0.3.0')
         
-        remotePath = '%s%s' % ('http://pypi.python.org/packages/source/p/pyflakes/', pyflakesFile)
-        mkdir(alePath('tmp'))
-        localDlPath = os.path.join(alePath('tmp'), pyflakesFile)
-        curlCmd = 'curl -o %s %s' % (localDlPath, remotePath)
-        print curlCmd
-        os.system(curlCmd)
-
-        extractPath = os.path.join(alePath('installed'), pyflakesversion)
-        mkdir(extractPath)
-
-        #todo: move %pyflakesversion% to alePath('installed/')
-        os.system('gzip -dc %s | tar xf -' % localDlPath)
+        downloadAndExtract('http://pypi.python.org/packages/source/p/pyflakes/pyflakes-0.3.0.tar.gz', extractPath)
