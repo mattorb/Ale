b37fe81180e6ed25660c0a944782c165b98a541e
msmith <msmith@msmiths-macbook-pro-17.local>
2009-11-30 14:07:14 -0600

    add browser launching to local server start

--- .ale/notinstalled/gae/commands/gae.py
+++ .ale/notinstalled/gae/commands/gae.py
@@ -7,10 +7,14 @@ from aleconfig import *
 from utils import *
 from ale.base import Command
 
+from subprocess import Popen
+
 gaefile = 'google_appengine_1.2.7.zip'
 gaeversion = 'google_appengine_1.2.7'
 remotePath = '%s%s' % ('http://googleappengine.googlecode.com/files/', gaefile)
 extractPath = join(join(join(alePath('installed'), 'gae'), 'pkgs'), gaeversion)
+
+
 
 class GaeCommand(Command):
     name = 'gae'
@@ -18,19 +22,16 @@ class GaeCommand(Command):
 
     def execute(self, args=None):
         if args and args[0].lower() == 'start':
-            os.system('%s/google_appengine/dev_appserver.py .' % extractPath)
+            p = Popen('%s/google_appengine/dev_appserver.py .' % extractPath, shell=True)
+#            os.system('%s/google_appengine/dev_appserver.py .' % extractPath)
+            import time
+            time.sleep(2)
+            Popen("open" + " http://localhost:8080", shell=True)
+            sts = os.waitpid(p.pid, 0)[1]
         else:
             print 'unknown command: %s' % args[0]
 
     def install(self, args=None):
         downloadAndExtract(remotePath, extractPath)
-        
-#class ServerCommand(Command):
-#    name = 'server'
-#    shorthelp = 'start stop local dev_appserver for gae:  server [start/stop]'
-
-#    def execute(self, args=None):
-#        if args[0].lower() != 'start':
-#            print 'Unknown app name %s' % args[0]
-#            return
-#            
+        os.system('chmod +x %s' % join(join(extractPath, 'google_appengine'), 'dev_appserver.py'))
+        os.system('chmod +x %s' % join(join(extractPath, 'google_appengine'), 'appcfg.py'))
