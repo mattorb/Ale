313d2f7074792ffa172760b944840601de5410dd
msmith <msmith@msmiths-macbook-pro-17.local>
2009-12-03 16:11:02 -0600

    add gae libs into python path for coverage run -- if they exist in ale or default install location

--- recipes_all/test/commands/coverage.py
+++ recipes_all/test/commands/coverage.py
@@ -5,8 +5,18 @@ import logging
 from os.path import join as join
 from aleconfig import alePath
 from ale.base import Command
+from ale.core import isCommandInstalled
 from subprocess import Popen
 from ale.utils import relpath
+
+def getGaeLibs():
+    if isCommandInstalled('gae'):
+        return [os.path.join(alePath('recipes_installed/gae/pkgs/google_appengine_1.2.7/google_appengine/'), d) for d in ('.', 'lib/django', 'lib/webob', 'lib/yaml/lib', 'lib/antlr3')]
+
+    if os.path.exists('/usr/local/google_appine'):
+        return [os.path.join('/usr/local/google_appine', d) for d in ('.', 'lib/django', 'lib/webob', 'lib/yaml/lib', 'lib/antlr3')]
+    
+    return []
 
 class NoseCoverageCommand(Command):
     name = 'coverage'
@@ -22,7 +32,7 @@ class NoseCoverageCommand(Command):
         command = join(join(noseroot, 'bin/'), 'nosetests')
         logging.info('Executing %s %s' % (relpath(command), arg))
 
-        pythonpath = '%s:%s' % (noseroot, coverageroot)
+        pythonpath = ':'.join([noseroot, coverageroot] + getGaeLibs())
 
         p = Popen([command, '--with-coverage', '--cover-erase', '--cover-inclusive', '--cover-exclude-package', 'nose', arg], env={"PYTHONPATH": pythonpath})
         sts = os.waitpid(p.pid, 0)[1]
