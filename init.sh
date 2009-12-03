efb0bbb8e8f19f93a9fd50453292f298d6dcc2dd
msmith <msmith@msmiths-macbook-pro-17.local>
2009-12-03 13:38:12 -0600

    adding more stuff to init

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
+echo Making sure we are using version control...
+git init
 echo Creating symlink to Ale
 ln -s .ale/recipes_installed/ale/main.py ale
+git add .ale .gitmodules
+git commit -a -m 'Pouring some ale on it'
