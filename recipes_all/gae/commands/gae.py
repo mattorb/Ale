61084592c979dc9f50ba6735432b3437f540284d
msmith <msmith@msmiths-macbook-pro-17.local>
2009-12-05 13:29:06 -0600

    add dash, logs, and data subcommands to gae to open the apps dashboard, logs viewer, and data viewer quickly

--- recipes_all/gae/commands/gae.py
+++ recipes_all/gae/commands/gae.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
+import re
 import logging
 from os.path import join as join
 from utils import downloadAndExtract, gitignore
@@ -14,11 +15,38 @@ gaeversion = 'google_appengine_1.2.7'
 remotePath = '%s%s' % ('http://googleappengine.googlecode.com/files/', gaefile)
 extractPath = join(join(join(alePath('recipes_installed'), 'gae'), 'pkgs'), gaeversion)
 
+def getAppId():
+    if not os.path.exists('app.yaml'):
+        logging.error('No app engine application lives here.  (no app.yaml).')
+        return None
+    p = re.compile('application: (.*)')
+    
+    appyamllines = open('app.yaml').read().split('\n')
+    
+    appId = None
+    for line in appyamllines:
+        match = p.match(line)
+        if match:
+            appId = match.group(1)
+            break;
+    
+    return appId
+
 class GaeCommand(Command):
     name = 'gae'
     shorthelp = 'google app engine .ale'
 
     def execute(self, args=None):
+        if not args:
+            print 'Syntax: ale gae [subcommand]'
+            print '   Available subcommands:'
+            print '   start         -- start the local dev_appserver'
+            print '   deploy        -- deploy to the hosted gae app'
+            print '   dash          -- open the dashboard for the hosted gae app'
+            print '   logs          -- open the dashboard (on logs tab) for hosted gae app'
+            print '   data          -- open the dashboard (on data tab) for hosted gae app'
+            return
+        
         if args and args[0].lower() == 'start':
             #--allow_skipped_files shouldn't be necesary, but don't have another work around yet...need a patch??
             p = Popen('%s/google_appengine/dev_appserver.py --allow_skipped_files .' % extractPath, shell=True)
@@ -27,13 +55,22 @@ class GaeCommand(Command):
             Popen("open" + " http://localhost:8080", shell=True)
             sts = os.waitpid(p.pid, 0)[1]
         elif args and args[0].lower() == 'deploy':
-            #todo: check for uncomitted changes and offer to commit them
+            #todo: check for uncomitted changes and warn?
             os.system('%s/google_appengine/appcfg.py update .' % extractPath)
+        elif args and args[0].lower() == 'dash':
+            appId = getAppId()
+            if appId:
+                os.system('open http://appengine.google.com/dashboard?app_id=%s' % appId)
+        elif args and args[0].lower() == 'logs':
+            appId = getAppId()
+            if appId:
+                os.system('open http://appengine.google.com/logs?app_id=%s' % appId)
+        elif args and args[0].lower() == 'data':
+            appId = getAppId()
+            if appId:
+                os.system('open http://appengine.google.com/datastore/explorer?app_id=%s' % appId)
         else:
-            if args:
-                logging.error('unknown command: %s' % args[0])
-            else:
-                print 'try "ale gae start"'
+            logging.error('unknown command: %s' % args[0])
 
     def install(self, args=None):
         downloadAndExtract(remotePath, extractPath)
